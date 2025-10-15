83f643cf08311be7b8924ca6171da5895001f2ae
t343s <t343s@PC-01>
2025-10-15 19:10:26 +0300

    сделал JsonToGeolocation

--- Model/DataType/Geolocation.cs
+++ Model/DataType/Geolocation.cs
@@ -2,7 +2,7 @@
 {
     public class Geolocation
     {
-        public required double Latitude { get; init; }  
-        public required double Longitude { get; init; }
+        public required string Latitude { get; init; }  
+        public required string Longitude { get; init; }
     }
 }
---
+++ Utils/Parsers/JsonToGeolocation.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using ConsoleCS_WheatherReceiver.Model.DataType;
+
+namespace ConsoleCS_WheatherReceiver.Utils.Parsers
+{
+    public static class JsonToGeolocation
+    {
+        public static Geolocation GetGeolocation(string json)
+        {
+            Geolocation geolocation;
+
+            using JsonDocument doc = JsonDocument.Parse(json);
+            var root = doc.RootElement;
+
+            string latitude = root.GetProperty("lat").ToString();
+            string longitude = root.GetProperty("lon").ToString();
+
+            return geolocation = new Geolocation() 
+            {
+                Latitude = latitude,
+                Longitude = longitude
+            };
+        }
+    }
+}
