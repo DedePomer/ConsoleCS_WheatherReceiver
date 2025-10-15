acfacd90b9dac84880fed7a6c4433a1d06e8b562
t343s <t343s@PC-01>
2025-10-15 19:33:32 +0300

    изменил ClassicPage

--- Pages/ClassicPage.cs
+++ Pages/ClassicPage.cs
@@ -4,10 +4,16 @@ namespace ConsoleCS_WheatherReceiver.Pages
 {
     public class ClassicPage : AbstractPage
     {
+
         public ClassicPage(IWheatherService wheatherService)
             : base(wheatherService)
-        {
+        {            
+        }
 
+        public async Task Open()
+        {   
+            string temperature = (await _wheatherService.GetWheatherAsync("Moscow")).Temperature;
+            Console.WriteLine($"Температура в Москве {temperature}");
         }
     }
 }
