ebe2953dc682ac037d8d7940b076311bfebca553
t343s <t343s@PC-01>
2025-10-15 15:53:30 +0300

    настроил DI

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
-﻿namespace ConsoleCS_WheatherReceiver
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace ConsoleCS_WheatherReceiver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Configuration.AddJsonFile("Utils\\appsettings.json");
+
+            var logPath = builder.Configuration["LogPath"];
+            ArgumentNullException.ThrowIfNullOrEmpty(logPath, nameof(logPath));
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(logPath)
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog();
+
+            AddServices(builder.Services);
+
+            var app = builder.Build();
+            app.Run();
+        }
+
+        private static void AddServices(IServiceCollection services)
+        { 
+        
         }
     }
 }
