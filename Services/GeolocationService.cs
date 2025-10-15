910dffd9fec2d0be5c2b2814ec0ff17fd380163e
t343s <t343s@PC-01>
2025-10-15 17:43:38 +0300

    сделал сервис геолокации

---
+++ Services/GeolocationService.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleCS_WheatherReceiver.Model.DataType;
+
+namespace ConsoleCS_WheatherReceiver.Services
+{
+    public class GeolocationService : IGeolocationService
+    {
+        private HttpClient _httpClient;
+        public GeolocationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public Task<Geolocation> GetGeolocationAsync(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
