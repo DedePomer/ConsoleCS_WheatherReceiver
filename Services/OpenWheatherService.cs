c1a5d55a3d2ad910c3b0fe3b059d530fd47e4351
t343s <t343s@PC-01>
2025-10-15 17:35:25 +0300

    +

---
+++ Services/OpenWheatherService.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleCS_WheatherReceiver.Model.Interfaces;
+
+namespace ConsoleCS_WheatherReceiver.Services
+{
+    /// <summary>
+    /// Берём API с этого сайта https://openweathermap.org/api
+    /// </summary>
+    public class OpenWheatherService : IWheatherService
+    {
+        private HttpClient _httpClient;
+        public OpenWheatherService(HttpClient httpClient) 
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IWheather> GetWheatherAsync(string city)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
