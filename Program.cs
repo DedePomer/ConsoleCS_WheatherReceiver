fdde0ffe5cafb7a04d931edfb0f2c618a66bfdaa
t343s <t343s@PC-01>
2025-10-15 15:46:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleCS_WheatherReceiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
