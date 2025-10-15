eff34a7f43c950d3e181c13453931d0b57adc2e3
t343s <t343s@PC-01>
2025-10-15 17:49:32 +0300

    +

--- Pages/AbstractPage.cs
+++ Pages/AbstractPage.cs
@@ -1,11 +1,13 @@
-﻿namespace ConsoleCS_WheatherReceiver.Pages
+﻿using ConsoleCS_WheatherReceiver.Services;
+
+namespace ConsoleCS_WheatherReceiver.Pages
 {
     public abstract class AbstractPage
     {
-
-        protected AbstractPage()
-        { 
-        
+        protected IWheatherService _wheatherService;
+        protected AbstractPage(IWheatherService wheatherService)
+        {
+            _wheatherService = wheatherService;
         }
     }
 }
--- Pages/ClassicPage.cs
+++ Pages/ClassicPage.cs
@@ -1,7 +1,13 @@
-﻿namespace ConsoleCS_WheatherReceiver.Pages
+﻿using ConsoleCS_WheatherReceiver.Services;
+
+namespace ConsoleCS_WheatherReceiver.Pages
 {
     public class ClassicPage : AbstractPage
     {
+        public ClassicPage(IWheatherService wheatherService)
+            : base(wheatherService)
+        {
 
+        }
     }
 }
